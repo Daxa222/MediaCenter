0d3feea980bb8840ad7b4e176b3bacd8ff38b648
Дарья Соколюк <dariasokol.18.red@mail.ru>
2023-11-24 17:53:44 +0500

    Создание контекста данных, написание строки подключения к любому экземпляру MS SQL Server, связь контекста и строки в файле Program

---
+++ MediaCenter/Models/AppCtx.cs
@@ -0,0 +1,15 @@
+﻿using MediaCenter.Models.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaCenter.Models
+{
+    public class AppCtx : IdentityDbContext<User>
+    {
+        public AppCtx(DbContextOptions<AppCtx> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ MediaCenter/Models/Data/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MediaCenter.Models.Data
+{
+    public class User : IdentityUser
+    {
+        [Required(ErrorMessage = "Введите фамилию")]
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Имя")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Дата регистрации")]
+        public DateTime RegDate { get; set; }
+    }
+}
